a9fccc55fecccea3bc5048c00523d8a0aba41c47
Sam Alba <sam.alba@gmail.com>
2013-07-11 08:45:05 -0700

    Bumped version to 0.5.2

--- registry/app.py
+++ registry/app.py
@@ -6,7 +6,7 @@ import config
 from toolkit import response, gen_random_string
 
 
-VERSION = '0.5.1'
+VERSION = '0.5.2'
 app = Flask('docker-registry')
 cfg = config.load()
 loglevel = getattr(logging, cfg.get('loglevel', 'INFO').upper())
