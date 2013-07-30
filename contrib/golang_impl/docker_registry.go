5021fb84ce7b6e49a044ef703943a385d6f79f31
Flavio Castelli <fcastelli@suse.com>
2013-07-30 14:03:15 +0200

    Add pidfile option to the golang implementation
    This is required to handle docker-registry with startproc.

--- contrib/golang_impl/docker_registry.go
+++ contrib/golang_impl/docker_registry.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 )
 
 var logger = &Logger{}
@@ -11,11 +17,54 @@ var (
 	GITCOMMIT string
 )
 
+func createPidFile(pidFile string) error {
+	if pidString, err := ioutil.ReadFile(pidFile); err == nil {
+		pid, err := strconv.Atoi(string(pidString))
+		if err == nil {
+			if _, err := os.Stat(fmt.Sprintf("/proc/%d/", pid)); err == nil {
+				return fmt.Errorf("pid file found, ensure docker-registry is not running or delete %s", pidFile)
+			}
+		}
+	}
 
-func startServer(listenOn, dataDir string) {
+	file, err := os.Create(pidFile)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	_, err = fmt.Fprintf(file, "%d", os.Getpid())
+	return err
+}
+
+func removePidFile(pidFile string) {
+	if err := os.Remove(pidFile); err != nil {
+		logger.Error("Error removing %s: %s", pidFile, err)
+	}
+}
+
+func startServer(listenOn, dataDir string, pidFile string) {
 	logger.Info("using version ", GITCOMMIT)
 	logger.Info("starting server on ", listenOn)
 	logger.Info("using dataDir ", dataDir)
+	logger.Info("using pidFile", pidFile)
+
+	if err := createPidFile(pidFile); err != nil {
+		logger.Error(err)
+		os.Exit(1)
+	}
+	defer removePidFile(pidFile)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill, os.Signal(syscall.SIGTERM))
+	go func() {
+		sig := <-c
+		logger.Debug("Received signal '%v', exiting\n", sig)
+		removePidFile(pidFile)
+		os.Exit(0)
+	}()
+
 	if err := http.ListenAndServe(listenOn, NewHandler(dataDir)); err != nil {
 		logger.Error(err.Error())
 	}
@@ -29,11 +78,12 @@ func main() {
 	listenOn = flag.String("l", ":80", "Address on which to listen.")
 	dataDir = flag.String("d", "/data/docker_index", "Directory to store data in")
 	doDebug = flag.Bool("D", false, "set log level to debug")
+	pidFile := flag.String("p", "/var/run/docker-registry.pid", "File containing process PID")
 	flag.Parse()
 
 	logger.Level = INFO
 	if *doDebug {
 		logger.Level = DEBUG
 	}
-	startServer(*listenOn, *dataDir)
+	startServer(*listenOn, *dataDir, *pidFile)
 }
