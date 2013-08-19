c0fd74a523de81db98a21dfb16657b2a989b7e3a
James R. Carr (inside a VM har har) <james.r.carr@gmail.com>
2013-08-19 19:09:25 +0000

    I got the hookup.

--- docker-run.sh
+++ docker-run.sh
@@ -1,12 +1,20 @@
-#!/bin/sh
-if [ "$SETTINGS_FLAVOR" = "prod" ] then
-	sed -i /docker-registry/config.yml -e "s/s3_access_key: REPLACEME/s3_access_key: $AWS_ACCESS_KEY_ID"
-	sed -i /docker-registry/config.yml -e "s/s3_secret_key: REPLACEME/s3_secret_key: $AWS_SECRET_KEY"
-	sed -i /docker-registry/config.yml -e "s/s3_bucket: REPLACEME/s3_bucket: $S3_BUCKET"
-	sed -i /docker-registry/config.yml -e "s/^secret_key: REPLACEME/secret_key: $WORKER_SECRET_KEY"
+#!/bin/bash
+if [[ -z "$GUNICORN_WORKERS" ]]
+	then
+		GUNICORN_WORKERS=2
 fi
 
-gunicorn --access-logfile - --debug --max-requests 100 --graceful-timeout 3600 -t 3600 -k gevent -b 0.0.0.0:5000 -w 2 wsgi:application
+if [ "$SETTINGS_FLAVOR" = "prod" ] 
+	then
+		config=$(<config.yml); 
+		config=${config//s3_access_key: REPLACEME/s3_access_key: $AWS_ACCESS_KEY_ID}; 
+		config=${config//s3_secret_key: REPLACEME/s3_secret_key: $AWS_SECRET_KEY}; 
+		config=${config//s3_bucket: REPLACEME/s3_bucket: $S3_BUCKET}; 
+		config=${config//secret_key: REPLACEME/secret_key: $WORKER_SECRET_KEY}; 
+		printf '%s\n' "$config" >config.yml
+fi
+
+gunicorn --access-logfile - --debug --max-requests 100 --graceful-timeout 3600 -t 3600 -k gevent -b 0.0.0.0:5000 -w $GUNICORN_WORKERS wsgi:application
 
 
 
