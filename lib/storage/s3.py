8c5064551183cdb70a36e4717812823966dd9c17
Sam Alba <sam.alba@gmail.com>
2013-10-10 12:06:40 -0700

    Changed s3_secure option to default to False (which is the default in boto), not None

--- lib/storage/s3.py
+++ lib/storage/s3.py
@@ -101,10 +101,10 @@ class S3Storage(Storage):
 
     def __init__(self, config):
         self._config = config
-        self._s3_conn = \
-            boto.s3.connection.S3Connection(self._config.s3_access_key,
-                                            self._config.s3_secret_key,
-                                            is_secure=self._config.s3_secure)
+        self._s3_conn = boto.s3.connection.S3Connection(
+            self._config.s3_access_key,
+            self._config.s3_secret_key,
+            is_secure=(self._config.s3_secure is True))
         self._s3_bucket = self._s3_conn.get_bucket(self._config.s3_bucket)
         self._root_path = self._config.storage_path
 
