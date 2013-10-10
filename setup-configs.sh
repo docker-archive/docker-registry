baf0d57b208f5921278b5d85b694bdfd30918e84
Rusty Ross <consultant@rustyross.com>
2013-10-10 14:13:01 -0400

    minor oversight in setup-configs.sh

--- setup-configs.sh
+++ setup-configs.sh
@@ -12,6 +12,6 @@ if [ "$SETTINGS_FLAVOR" = "prod" ] ; then
     config=${config//s3_access_key: REPLACEME/s3_access_key: $AWS_ACCESS_KEY_ID};
     config=${config//s3_secret_key: REPLACEME/s3_secret_key: $AWS_SECRET_KEY};
     config=${config//s3_bucket: REPLACEME/s3_bucket: $S3_BUCKET};
-    config=${config//s3_bucket: REPLACEME/s3_bucket: ${S3_SECURE:-False}};
+    config=${config//s3_secure: REPLACEME/s3_secure: ${S3_SECURE:-False}};
     printf '%s\n' "$config" >config.yml
 fi
