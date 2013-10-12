f10a31444bdd56d53c07ca9cf1c701784800c804
Sam Alba <sam.alba@gmail.com>
2013-10-12 16:15:11 -0700

    Config cleaning, switch to gunicorn config file instead of cli args

--- registry/images.py
+++ registry/images.py
@@ -60,15 +60,12 @@ def get_image_layer(image_id, headers):
     try:
         accel_uri_prefix = cfg.nginx_x_accel_redirect
         path = store.image_layer_path(image_id)
-
         if accel_uri_prefix:
             if isinstance(store, LocalStorage):
                 accel_uri = '/'.join([accel_uri_prefix, path])
                 headers['X-Accel-Redirect'] = accel_uri
-
-                logger.debug('send accelerated {} ({})'.format(
+                logger.debug('send accelerated {0} ({1})'.format(
                     accel_uri, headers))
-
                 return flask.Response('', headers=headers)
             else:
                 logger.warn('nginx_x_accel_redirect config set,'
