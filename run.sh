366fc574290c4d2a5d3541b1b6dde560d9cf9043
Ian Miell <ian.miell@openbet.com>
2013-12-27 13:03:47 +0000

    Timeout env vars added to run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,13 @@ if [[ -z "$REGISTRY_PORT" ]] ; then
     REGISTRY_PORT=5000
 fi
 
+if [[ -z "$GUNICORN_GRACEFUL_TIMEOUT" ]] ; then
+    GUNICORN_GRACEFUL_TIMEOUT=3600
+fi
+
+if [[ -z "$GUNICORN_SILENT_TIMEOUT" ]] ; then
+    GUNICORN_SILENT_TIMEOUT=3600
+fi
 
 cd "$(dirname $0)"
-gunicorn --access-logfile - --debug --max-requests 100 --graceful-timeout 3600 -t 3600 -k gevent -b 0.0.0.0:$REGISTRY_PORT -w $GUNICORN_WORKERS wsgi:application
+gunicorn --access-logfile - --debug --max-requests 100 --graceful-timeout $GUNICORN_GRACEFUL_TIMEOUT -t $GUNICORN_SILENT_TIMEOUT -k gevent -b 0.0.0.0:$REGISTRY_PORT -w $GUNICORN_WORKERS wsgi:application
