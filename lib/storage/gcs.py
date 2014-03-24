d3a5e7f8c23aa75bb48accd9b8258e2a3f2e400a
Ekaterina Volkova <gloom@google.com>
2014-03-24 11:24:59 -0700

    tabs

--- lib/storage/gcs.py
+++ lib/storage/gcs.py
@@ -16,39 +16,39 @@ logger = logging.getLogger(__name__)
 
 
 def _UpdateSysPath(config):
-  import sys
+    import sys
 
-  cloud_sdk_path = config.cloud_sdk_path or '/google-cloud-sdk/'
-  gsutil_path =  cloud_sdk_path + 'platform/gsutil/'
-  gsutil_third_party_path = gsutil_path + 'third_party/'
+    cloud_sdk_path = config.cloud_sdk_path or '/google-cloud-sdk/'
+    gsutil_path =  cloud_sdk_path + 'platform/gsutil/'
+    gsutil_third_party_path = gsutil_path + 'third_party/'
 
-  # oauth2client
-  sys.path.append(cloud_sdk_path + 'lib/')
+    # oauth2client
+    sys.path.append(cloud_sdk_path + 'lib/')
 
-  # gslib.*
-  sys.path.append(gsutil_path)
+    # gslib.*
+    sys.path.append(gsutil_path)
 
-  # httplib2
-  sys.path.append(gsutil_third_party_path + 'httplib2/python2')
+    # httplib2
+    sys.path.append(gsutil_third_party_path + 'httplib2/python2')
 
-  # sock.py
-  sys.path.append(gsutil_third_party_path + 'httplib2/python2/httplib2')
+    # sock.py
+    sys.path.append(gsutil_third_party_path + 'httplib2/python2/httplib2')
 
-  sys.path.append(gsutil_third_party_path + 'retry-decorator')
+    sys.path.append(gsutil_third_party_path + 'retry-decorator')
 
 
 def _LoadOauth2Plugin():
-  import threading
+    import threading
 
-  try:
-    from gslib.third_party.oauth2_plugin import oauth2_plugin # flake8: noqa
-    from gslib.third_party.oauth2_plugin import oauth2_client
-    oauth2_client.token_exchange_lock = threading.Lock()
+    try:
+        from gslib.third_party.oauth2_plugin import oauth2_plugin # flake8: noqa
+        from gslib.third_party.oauth2_plugin import oauth2_client
+        oauth2_client.token_exchange_lock = threading.Lock()
 
-  except ImportError:
-    logger.error('To use OAuth 2.0 with Google Cloud Storage install '
-                 'Google Cloud SDK and set cloud_sdk_path in config.yml. '
-                 'For more info see https://developers.google.com/cloud/sdk/')
+    except ImportError:
+        logger.error('To use OAuth 2.0 with Google Cloud Storage install '
+                     'Google Cloud SDK and set cloud_sdk_path in config.yml. '
+                     'For more info see https://developers.google.com/cloud/sdk/')
 
 
 class GSStorage(BotoStorage):
@@ -58,11 +58,11 @@ class GSStorage(BotoStorage):
 
     def makeConnection(self):
         if self._config.oauth2 is True:
-          _UpdateSysPath(self._config)
-          _LoadOauth2Plugin()
+            _UpdateSysPath(self._config)
+            _LoadOauth2Plugin()
 
-          uri = boto.storage_uri(self._config.boto_bucket, 'gs')
-          return uri.connect()
+            uri = boto.storage_uri(self._config.boto_bucket, 'gs')
+            return uri.connect()
 
         return boto.gs.connection.GSConnection(
             self._config.gs_access_key,
