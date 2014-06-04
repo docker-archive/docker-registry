b40b3ad57706fa6874e47f22b2f2c3be41339607
Vincent Batts <vbatts@redhat.com>
2014-06-04 15:55:47 -0400

    adding tests for TarSum
    Docker-DCO-1.1-Signed-off-by: Vincent Batts <vbatts@redhat.com> (github: vbatts)

--- tests/test_tarfile.py
+++ tests/test_tarfile.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os, sys
+import os
 
 import base
 from docker_registry.lib import checksums
@@ -9,15 +9,6 @@ from docker_registry.lib import xtarfile
 tarfile = xtarfile.tarfile
 
 class TestTarfile(base.TestCase):
-
-    def setUp(self):
-        self.header_fields = ('name', 'mode', 'uid', 'gid', 'size', 'mtime',
-                              'type', 'linkname', 'uname', 'gname', 'devmajor',
-                              'devminor')
-        #self.store = storage.load(kind='file')
-        #self.filenames = list(comp(5, rndstr))
-        pass
-
     def test_headers(self):
         expected = { 
                 "46af0962ab5afeb5ce6740d4d91652e69206fc991fd5328c1a94d364ad00e457/layer.tar": {
@@ -86,4 +77,25 @@ class TestTarfile(base.TestCase):
                     assert e["pax"][attr] == member.pax_headers[attr], "in %s:%s, expected %s of %s, but got %s" % (file, member.path, attr, e["pax"][attr], member.pax_headers[attr])
 
             assert member_count == len(expected[file])
+            layer_fh.close()
+
+    def test_tarsum(self):
+        expected = { 
+                "46af0962ab5afeb5ce6740d4d91652e69206fc991fd5328c1a94d364ad00e457": "tarsum+sha256:e58fcf7418d4390dec8e8fb69d88c06ec07039d651fedd3aa72af9972e7d046b",
+                "511136ea3c5a64f264b78b5433614aec563103b4d4702f3ba7d4d2698e22c158": "tarsum+sha256:ac672ee85da9ab7f9667ae3c32841d3e42f33cc52c273c23341dabba1c8b0c8b",
+                "xattr": "tarsum+sha256:e86f81a4d552f13039b1396ed03ca968ea9717581f9577ef1876ea6ff9b38c98",
+                }
+        for layer in expected.keys():
+            layer_fh = open(os.path.join(base.data_dir, layer, "layer.tar"))
+            json_fh = open(os.path.join(base.data_dir, layer, "json"))
+
+            tarsum = checksums.TarSum(json_fh.read())
+            tar = tarfile.open(mode='r|*', fileobj=layer_fh)
+            for member in tar:
+                tarsum.append(member, tar)
+            sum = tarsum.compute()
+            assert expected[layer] == sum, "layer %s, expected [%s] but got [%s]" % (layer, expected[layer], sum)
+
+            layer_fh.close()
+            json_fh.close()
 
