598a3b2041c39a4baacc0a5f3ac38a50495836f6
Mangled Deutz <olivier@webitup.fr>
2014-06-04 15:40:38 -0700

    Tentative fix for lru cache config
    Docker-DCO-1.1-Signed-off-by: Mangled Deutz <olivier@webitup.fr> (github: dmp42)

--- docker_registry/lib/cache.py
+++ docker_registry/lib/cache.py
@@ -11,19 +11,18 @@ from . import config
 
 # Default options
 
-redis_opts = {
-    'host': 'localhost',
-    'port': 6379,
-    'db': 0,
-    'password': None
-}
 redis_conn = None
 cache_prefix = None
 
 
 def init():
-    global redis_conn, cache_prefix
     cfg = config.load()
+    enable_redis_cache(cfg)
+    enable_redis_lru(cfg)
+
+
+def enable_redis_cache(cfg):
+    global redis_conn, cache_prefix
     cache = cfg.cache
     if not cache:
         return
@@ -31,6 +30,12 @@ def init():
     logging.info('Enabling storage cache on Redis')
     if not isinstance(cache, dict):
         cache = {}
+    redis_opts = {
+        'host': 'localhost',
+        'port': 6379,
+        'db': 0,
+        'password': None
+    }
     for k, v in cache.iteritems():
         redis_opts[k] = v
     logging.info('Redis config: {0}'.format(redis_opts))
@@ -40,7 +45,24 @@ def init():
                                    password=redis_opts['password'])
     cache_prefix = 'cache_path:{0}'.format(cfg.get('storage_path', '/'))
 
-    # Enable the LRU as well
+
+def enable_redis_lru(cfg):
+    cache = cfg.cache_lru
+    if not cache:
+        return
+    logging.info('Enabling lru cache on Redis')
+    if not isinstance(cache, dict):
+        cache = {}
+    redis_opts = {
+        'host': 'localhost',
+        'port': 6379,
+        'db': 0,
+        'password': None
+    }
+    for k, v in cache.iteritems():
+        redis_opts[k] = v
+
+    logging.info('Redis lru config: {0}'.format(redis_opts))
     lru.init(
         host=redis_opts['host'],
         port=int(redis_opts['port']),
