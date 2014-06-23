4b40c3f9320eca960e58afe7ec66bef357269069
Mangled Deutz <olivier@webitup.fr>
2014-06-23 13:27:28 +0200

    New config class
    Docker-DCO-1.1-Signed-off-by: Mangled Deutz <olivier@webitup.fr> (github: dmp42)

--- docker_registry/lib/config.py
+++ docker_registry/lib/config.py
@@ -4,52 +4,72 @@ import os
 import rsa
 import yaml
 
+from docker_registry.core import compat
 from docker_registry.core import exceptions
 
 
 class Config(object):
+    """A simple config class that:
+     * gives properties access through either items or attributes
+     * enforce types (thanks to yaml)
+     * interpolate from ENV
+    """
 
-    def __init__(self, config):
-        self._config = config
+    def __init__(self, config=''):
+        try:
+            # Config is kept as-is...
+            self._config = config
+            # ... save Strings, that are yaml loaded
+            if isinstance(config, compat.basestring):
+                self._config = yaml.load(config)
+        except Exception as e:
+            # Failed yaml loading? Stop here!
+            raise exceptions.ConfigError(
+                'Config is not valid yaml (%s): \n%s' % (e, config))
 
     def __repr__(self):
         return repr(self._config)
 
+    def __dir__(self):
+        return self._config.keys()
+
+    # Python 2.6 and below need this
+    @property
+    def __members__(self):
+        return self._config.keys()
+
+    @property
+    def __methods__(self):
+        return []
+
     def __getattr__(self, key):
-        if key in self._config:
-            return self._config[key]
+        # Unset keys return None
+        if key not in self._config:
+            return None
+            # raise exceptions.ConfigError("No such attribute: %s" % key)
+        result = self._config[key]
+        # Dicts are rewrapped inside a Config object
+        if isinstance(result, dict):
+            return Config(result)
+        # Strings starting with `_env:' get evaluated
+        if isinstance(
+                result, compat.basestring) and result.startswith('_env:'):
+            result = result.split(':', 2)
+            varname = result[1]
+            vardefault = '' if len(result) < 3 else result[2]
+            try:
+                result = yaml.load(os.environ.get(varname, vardefault))
+            except Exception as e:
+                raise exceptions.ConfigError(
+                    'Config `%s` (value: `%s`) is not valid: %s' % (
+                        varname, e, result))
+        return result
 
-    def get(self, *args, **kwargs):
-        return self._config.get(*args, **kwargs)
+    def __getitem__(self, key):
+        return getattr(self, key)
 
-
-def _walk_object(obj, callback):
-    if not hasattr(obj, '__iter__'):
-        return callback(obj)
-    obj_new = {}
-    if isinstance(obj, dict):
-        for i, value in obj.iteritems():
-            value = _walk_object(value, callback)
-            if value or value == '':
-                obj_new[i] = value
-        return obj_new
-    for i, value in enumerate(obj):
-        value = _walk_object(value, callback)
-        if value or value == '':
-            obj_new[i] = value
-    return obj_new
-
-
-def convert_env_vars(config):
-    def _replace_env(s):
-        if isinstance(s, basestring) and s.startswith('_env:'):
-            parts = s.split(':', 2)
-            varname = parts[1]
-            vardefault = None if len(parts) < 3 else parts[2]
-            return os.environ.get(varname, vardefault)
-        return s
-
-    return _walk_object(config, _replace_env)
+    def __contains__(self, key):
+        return key in self._config
 
 
 _config = None
@@ -59,8 +79,10 @@ def load():
     global _config
     if _config is not None:
         return _config
-    data = None
+
+    flavor = os.environ.get('SETTINGS_FLAVOR', 'dev')
     config_path = os.environ.get('DOCKER_REGISTRY_CONFIG', 'config.yml')
+
     if not os.path.isabs(config_path):
         config_path = os.path.join(os.path.dirname(__file__), '../../',
                                    'config', config_path)
@@ -70,29 +92,25 @@ def load():
         raise exceptions.FileNotFoundError(
             'Heads-up! File is missing: %s' % config_path)
 
-    try:
-        data = yaml.load(f)
-    except Exception:
-        raise exceptions.ConfigError(
-            'Config file (%s) is not valid yaml' % config_path)
+    _config = Config(f.read())
+    if flavor:
+        _config = _config[flavor]
+        _config.flavor = flavor
 
-    config = data.get('common', {})
-    flavor = os.environ.get('SETTINGS_FLAVOR', 'dev')
-    config.update(data.get(flavor, {}))
-    config['flavor'] = flavor
-    config = convert_env_vars(config)
-    if 'privileged_key' in config:
+    if _config.privileged_key:
         try:
-            f = open(config['privileged_key'])
+            f = open(_config.privileged_key)
         except Exception:
             raise exceptions.FileNotFoundError(
-                'Heads-up! File is missing: %s' % config['privileged_key'])
+                'Heads-up! File is missing: %s' % _config.privileged_key)
 
         try:
-            config['privileged_key'] = rsa.PublicKey.load_pkcs1(f.read())
+            _config.privileged_key = rsa.PublicKey.load_pkcs1(f.read())
         except Exception:
             raise exceptions.ConfigError(
-                'Key at %s is not a valid RSA key' % config['privileged_key'])
+                'Key at %s is not a valid RSA key' % _config.privileged_key)
 
-    _config = Config(config)
+    if _config.index_endpoint:
+        _config.index_endpoint = _config.index_endpoint.strip('/')
+
     return _config
