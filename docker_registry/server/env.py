884b418f22877ad55d79335c21c7ac86b5e69162
Mangled Deutz <olivier@webitup.fr>
2014-06-23 14:35:45 +0200

    Type from environment properly (like Config)
    Docker-DCO-1.1-Signed-off-by: Mangled Deutz <olivier@webitup.fr> (github: dmp42)

--- docker_registry/server/env.py
+++ docker_registry/server/env.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import os
+import yaml
 
 __all__ = ['source']
 
 _DEFAULT = {
-    'REGISTRY_PORT': 5000,
+    'REGISTRY_PORT': '5000',
     'REGISTRY_HOST': '0.0.0.0',
     'SETTINGS_FLAVOR': 'dev',
-    'GUNICORN_WORKERS': 4,
-    'GUNICORN_GRACEFUL_TIMEOUT': 3600,
-    'GUNICORN_SILENT_TIMEOUT': 3600
+    'GUNICORN_WORKERS': '4',
+    'GUNICORN_GRACEFUL_TIMEOUT': '3600',
+    'GUNICORN_SILENT_TIMEOUT': '3600'
 }
 
 
-def source(key, override=None):
-    return os.environ.get(key, _DEFAULT[key] if key in _DEFAULT else override)
+def source(key, override='None'):
+    # Using yaml gives us proper typage
+    return yaml.load(
+        os.environ.get(key, _DEFAULT[key] if key in _DEFAULT else override))
