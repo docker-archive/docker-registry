d4cfa790e0b7909f2fb39e1e6d157a8c977b8169
Olivier Gambier <olivier@docker.com>
2014-09-04 16:30:14 -0700

    Better bugsnag exception handling
    Docker-DCO-1.1-Signed-off-by: Olivier Gambier <olivier@docker.com> (github: dmp42)

--- docker_registry/extras/bugsnag.py
+++ docker_registry/extras/bugsnag.py
@@ -6,15 +6,18 @@ import os
 def boot(application, api_key, flavor, version):
     # Configure bugsnag
     if api_key:
-        import bugsnag
-        import bugsnag.flask
+        try:
+            import bugsnag
+            import bugsnag.flask
 
-        root_path = os.path.abspath(
-            os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-        bugsnag.configure(api_key=api_key,
-                          project_root=root_path,
-                          release_stage=flavor,
-                          notify_release_stages=[flavor],
-                          app_version=version
-                          )
-        bugsnag.flask.handle_exceptions(application)
+            root_path = os.path.abspath(
+                os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+            bugsnag.configure(api_key=api_key,
+                              project_root=root_path,
+                              release_stage=flavor,
+                              notify_release_stages=[flavor],
+                              app_version=version
+                              )
+            bugsnag.flask.handle_exceptions(application)
+        except Exception as e:
+            raise Exception('Failed to init bugsnag agent %s' % e)
