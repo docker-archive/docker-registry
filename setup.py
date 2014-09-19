99a1dfdcc22a26347f84191d2d8441dc179b1a2b
Olivier Gambier <olivier@docker.com>
2014-09-19 15:27:53 -0700

    Fix forgotten package
    - extras is now included in packages declaration
    - alphabetically ordered now to avoid more churn

--- setup.py
+++ setup.py
@@ -47,11 +47,13 @@ requirements.insert(0, 'docker-registry-core>=2,<3')
 # Explicit packages list to avoid setup_tools funkyness
 packages = ['docker_registry',
             'docker_registry.drivers',
-            'docker_registry.server',
+            'docker_registry.extensions',
+            'docker_registry.extras',
             'docker_registry.lib',
-            'docker_registry.storage',
             'docker_registry.lib.index',
-            'docker_registry.extensions']
+            'docker_registry.server',
+            'docker_registry.storage',
+            ]
 
 namespaces = ['docker_registry', 'docker_registry.drivers']
 
